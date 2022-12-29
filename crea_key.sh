8222d66d570d765e5fe49456bb7aa64359372e6f
pentolone <luke.romano@gmail.com>
2022-12-29 21:28:53 +0100

    Try to keep in order

---
+++ crea_key.sh
@@ -0,0 +1,8 @@
+#
+#	Crea Key
+#
+#	File name: crea_key.sh
+#	Description: create a key based on hostname, secret, database and period
+#
+#############################################################################
+php -r 'echo md5(("8928d71285a0pentoloneunitalsimb2023-12");'
